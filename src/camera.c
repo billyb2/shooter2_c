a2a57192a6b2075530594feb0206a30a4fd4105d
Willaim Batista <bootlegbilly@protonmail.ch>
2022-08-18 11:12:16 -0400

    fix(camera): camera no longer jumps on small maps
    Made a slight fix to the camera logic where now if the screen is bigger
    than the map, it just displays the map normally rather than trying to
    scroll.

--- src/camera.c
+++ src/camera.c
@@ -1,4 +1,3 @@
-#include <stdint.h>
 #include <stdio.h>
 
 #include "camera.h"
@@ -8,27 +7,45 @@
 extern const int SCREEN_WIDTH;
 extern const int SCREEN_HEIGHT;
 
-void move_camera(Camera2D* camera, const Map* map, uint16_t pos_x, uint16_t pos_y) {
-	if (pos_x < SCREEN_WIDTH / 2) {
-		camera->target.x = (float)SCREEN_WIDTH / 2.0;
+void move_camera(Camera2D* camera, const Map* map, float pos_x, float pos_y) {
+	bool screen_bigger_than_map_x, screen_bigger_than_map_y = false;
 
-	} else if (pos_x > map->size_x - (SCREEN_WIDTH / 2)) {
-		camera->target.x = (float)map->size_x - (float)SCREEN_WIDTH / 2.0;
-
-	} else {
-		camera->target.x = (float)pos_x;
+	if (SCREEN_WIDTH >= map->size_x) {
+		camera->target.x = SCREEN_WIDTH / 2.0;
+		screen_bigger_than_map_x = true;
 
 	}
-	
 
-	if (pos_y < SCREEN_HEIGHT / 2) {
-		camera->target.y = (float)SCREEN_HEIGHT / 2.0;
+	if (SCREEN_HEIGHT >= map->size_y) {
+		camera->target.y = SCREEN_HEIGHT / 2.0;
+		screen_bigger_than_map_y = true;
+	}
 
-	} else if (pos_y > map->size_y - (SCREEN_HEIGHT / 2)) {
-		camera->target.y = (float)map->size_y - (float)SCREEN_HEIGHT / 2.0;
+	if (!screen_bigger_than_map_x) {
+		if (pos_x < SCREEN_WIDTH / 2.0) {
+			camera->target.x = (float)SCREEN_WIDTH / 2.0;
 
-	} else {
-		camera->target.y = (float)pos_y;
+		} else if (pos_x > map->size_x - (SCREEN_WIDTH / 2.0)) {
+			camera->target.x = (float)map->size_x - (float)SCREEN_WIDTH / 2.0;
+
+		} else {
+			camera->target.x = (float)pos_x;
+
+		}
+
+	}	
+
+	if (!screen_bigger_than_map_y) {
+		if (pos_y < SCREEN_HEIGHT / 2.0) {
+			camera->target.y = (float)SCREEN_HEIGHT / 2.0;
+
+		} else if (pos_y > map->size_y - (SCREEN_HEIGHT / 2.0)) {
+			camera->target.y = (float)map->size_y - (float)SCREEN_HEIGHT / 2.0;
+
+		} else {
+			camera->target.y = (float)pos_y;
+
+		}
 
 	}
 
