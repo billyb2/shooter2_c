0619bbbd8ad5ecc0f3ef61256456692f1000072c
Willaim Batista <bootlegbilly@protonmail.ch>
2022-08-21 10:53:31 -0400

    feat(ui): added a basic UI to the game
    This helps player to know their ammo count and health amounts, allowing
    them to be more critical in the choices they make while playing.

--- src/render_raylib.c
+++ src/render_raylib.c
@@ -1,29 +1,26 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "include/raylib.h"
 #include "map.h"
 #include "projectile.h"
 #include "player.h"
-//#include "render.h"
+#include "render.h"
+
+extern int SCREEN_WIDTH;
+extern int SCREEN_HEIGHT;
 
 void render(Camera2D camera, const Player* players, uint8_t num_players, const Projectile* projectiles, uint16_t num_projectiles, const Map* map) {
 	BeginDrawing();
 		ClearBackground(RAYWHITE);
+
 		BeginMode2D(camera);
 
 		for (uint8_t i = 0; i < num_players; i += 1) {
 			const Player* player = &players[i];
 
-			Color color = {
-				.r = 255,
-				.g = 0,
-				.b = 0,
-				.a = ((float)(player->health) / 500.0) * 255.0,
-
-			};
-
-			DrawCircle(player->pos_x, player->pos_y, PLAYER_SIZE, color);
+			DrawCircle(player->pos_x, player->pos_y, PLAYER_SIZE, RED);
 
 		}
 
@@ -42,6 +39,65 @@ void render(Camera2D camera, const Player* players, uint8_t num_players, const P
 			}
 
 		}
+
+		EndMode2D();
+
+		// Start drawing the UI
+		
+		// First, draw the health bar
+		Color health_color;
+
+		if (players[0].health > ((float)PLAYER_MAX_HEALTH * 2.0 / 3.0)) {
+			health_color = GREEN;
+
+		} else if (players[0].health > PLAYER_MAX_HEALTH / 3.0) {
+			health_color = YELLOW;
+
+		} else {
+			health_color = RED;
+
+		}
+
+		const Color health_bar_color =  {
+			.r = PURPLE.r,
+			.g = PURPLE.g,
+			.b = PURPLE.b,
+			.a = 128,
+
+		};
+
+		const Color health_bar_outline_color =  {
+			.r = BLACK.r,
+			.g = BLACK.g,
+			.b = BLACK.b,
+			.a = 128,
+
+		};
+
+		DrawText("Health", SCREEN_WIDTH - 100, 0, 20, health_bar_outline_color);
+		Rectangle health_bar_rect = {
+			.x = SCREEN_WIDTH - 120,
+			.y = 25.0,
+			.width = 125.0 * ((float)players[0].health / (float)PLAYER_MAX_HEALTH),
+			.height = 20.0,
+
+		};
+
+		DrawRectangleRec(health_bar_rect, health_bar_color);
+		DrawRectangleLinesEx(health_bar_rect, 3.5, health_bar_outline_color);
+
+		// Draw the ammo count
+		char ammo_count_text[16] = { 0 };
+
+		if (players[0].reloading) {
+			memcpy(ammo_count_text, "Reloading", 9);
+
+		} else {
+			sprintf(ammo_count_text, "Ammo: %u / %u", players[0].ammo, get_ammo_count(players[0].weapon));
+
+		}
+
+		DrawText(ammo_count_text, SCREEN_WIDTH - 125, 50, 20, health_bar_outline_color);
 
 	EndDrawing();
 
